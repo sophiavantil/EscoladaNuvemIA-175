90b78b9c60b0eb5d7c3c87f4436d8b5e7a59068a
sophiavantil <vantil26@gmail.com>
2025-07-19 00:04:58 -0300

    Atividade prática 01 - Módulo IA - Escola da Nuvem

---
+++ atividade-pratica-01/calculador_preco_total.py
@@ -0,0 +1,10 @@
+produto = "Cadeira infantil"
+preco_unitario = 12.40
+quantidade = 3 
+
+preco_total = preco_unitario * quantidade
+
+print(f"Produto: {produto}")
+print(f"Preço unitário: R${preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Valor total: {preco_total:.2f}")
---
+++ atividade-pratica-01/calculadora_numero_inteiro.py
@@ -0,0 +1,8 @@
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = int(input("Digite o terceiro valor: "))
+d = int(input("Digite o quarto valor: "))
+
+diferenca = (a * b - c * d)
+
+print(f"DIFERENCA = {diferenca}")
